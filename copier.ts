8cc89141a7a7c96c77a3498f881cff9a5d39d27d
donomess <89869979+donomess@users.noreply.github.com>
2021-09-16 23:20:36 -0400

    Added failed typescript function, github was not working

---
+++ copier.ts
@@ -0,0 +1,11 @@
+/*Found the original source for this copier on 
+https://stackoverflow.com/questions/40958613/how-to-copy-url-on-button-click, 
+added a alert popup to it so I didn't just straight up copy something off the internet. */
+
+/*function urlCopier(){
+    var Url = document.createElement("copiedText");
+    Url.innerHTML = window.location.href;
+    copiedText = Url.createTextRange();
+    //I CAN NOT GET THIS TO WORK no matter what I try, I tried for 
+    //like an hour and a half with no success. Sorry.
+}*/
