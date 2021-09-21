f261c48932806664842d87b7b47531ed95033bdd
Donovan Messer <donovanmess01@gmail.com>
2021-09-21 15:20:26 -0400

    Attempt to fix button 4

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function copyurl(link){ //Allows a url to be copied to clipboard on click.
     // Referenced from: https://orclqa.com/copy-url-clipboard/
-    var copiedUrl = document.body.appendChild(document.createElement("copied"));
+    var copiedUrl = document.body.appendChild(document.createElement("input"));
     copiedUrl.value = window.location.href;
     copiedUrl.focus();
     copiedUrl.select();
